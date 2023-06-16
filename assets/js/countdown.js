e8dc823268472f359f865b73be4062b8b1540dd4
Erbil Eren <erbil.eren@ip-10-1-2-159.eu-central-1.compute.internal>
2023-06-16 16:56:15 +0300

    init

---
+++ assets/js/countdown.js
@@ -0,0 +1,41 @@
+window.onload = function (e) {
+  document.getElementById("future-trip").hidden = true;
+
+  // Set the date we're counting down to
+  var countDownDate = new Date("Sep 9, 2023 00:00:01").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("countdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "Yo-ho-ho!";
+
+      // Currenct and upcoming trip set for the finished countdown
+      document.getElementById("latest-trip").classList.add("archived");
+      document.getElementById("latest-trip").classList.remove("active");
+      document.getElementById("latest-trip").childNodes[0].classList.remove("fa-fade");
+      document.getElementById("latest-trip").removeAttribute("id");
+
+      document.getElementById("future-trip").hidden = false;
+    }
+  }, 1000);
+};
